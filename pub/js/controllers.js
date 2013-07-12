cb604aada3f91f4584674d7f9e61e1426a57ae3a
Ketema Harris <ketema@midnightoilconsulting.com>
2013-07-11 23:49:55 -0400

    [#53196411] Now able to pull form values on click
    By removing the function paramters and simply grabbing the form values
    within the click function I was able to get current values at time of
    click.  Next is to put a little logic around if the username and
    password are blank because I do not want to call the REST api without
    credentials.  Then call the API to get the person token, store it the
    use it for all subsequent requests.  Any time the credentials change we
    use the new token for all subsequent requests.

--- pub/js/controllers.js
+++ pub/js/controllers.js
@@ -4,14 +4,14 @@
 
 angular.module('ptStats.controllers', []).
   controller('ptWhoamICtrl', [ '$scope', function($scope){
-    $scope.identify = function(username, password){
+    $scope.person = {};
+    $scope.identify = function(){
         //I want to read the form fields and call a rest api
         //curl -u $USERNAME:$PASSWORD -X GET https://www.pivotaltracker.com/services/v4/me
         //substitute the form values make the call and then convert the returned xml into 
-        //a user object whose token is used for all subsequent requests.
-        //
-        //First thing I have to figure out is how to attach this function to the click event of that
-        //form.
-        alert('username: '+username+'\npassword: '+password);
+        //a person object whose token is used for all subsequent requests.
+        $scope.person.username = angular.element('#username').val();
+        $scope.person.password = angular.element('#password').val();
+        alert('username: ' + $scope.person.username + '\npassword: ' + $scope.person.password);
     };
   }]);
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -3,15 +3,12 @@
 /* jasmine specs for controllers go here */
 
 describe('controllers', function(){
-    describe('ptWhoamICtrl', function(){
-        it('should receive two parameters', function() {
-            var scope = {},
-                ctrl = new ptWhoamICtrl(scope);
-            expect(scope.person).not.toBe(null);
+  beforeEach(module('ptStats.controllers'));
+    it('should receive two parameters', inject(function() {
+        expect(person).not.toBe(null);
 //https://www.pivotaltracker.com/help/api?version=v4#retrieve_me
 //I want this model to take username and password from form then call [ivotal
 //REST api the greate a person object.  this person's token is what will be used for all subsequent
 //requests to the api.  to change token just exchange credentials
-        });  
-    });
+    }));  
 });
