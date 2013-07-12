35a830bd1029a513b3bbd23818bf77c12c5b4193
Ketema Harris <ketema@midnightoilconsulting.com>
2013-07-12 13:39:01 -0400

    [#53196411] Moved past injecting Base64 service
    I still am not completely understanding how the DI works and the JS
    syntax is definitely not my favorite.  I need a better understanding of
    the Angular order of operations and specifically how services and
    modules are registered and then found and loaded.  Currently am stuck
    wondering why the http is not available.

--- pub/js/app.js
+++ pub/js/app.js
@@ -2,9 +2,9 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('ptStats', ['ptStats.filters', 'ptStats.services', 'ptStats.directives', 'ptStats.controllers']).
-  config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/users', {templateUrl: 'partials/users.html', controller: 'ptAPICtrl'});
-    $routeProvider.when('/sprints', {templateUrl: 'partials/sprints.html', controller: 'ptAPICtrl'});
-    $routeProvider.otherwise({redirectTo: '/view1'});
-  }]);
+angular.module('ptStats', [
+        'ptStats.filters', 
+        'ptStats.services', 
+        'ptStats.directives', 
+        'ptStats.controllers']
+);
--- pub/js/controllers.js
+++ pub/js/controllers.js
@@ -2,16 +2,25 @@
 
 /* Controllers */
 
-angular.module('ptStats.controllers', []).
-  controller('ptWhoamICtrl', [ '$scope', function($scope){
+angular.module('ptStats.controllers', ['ptStats.services']).
+  controller('ptWhoamICtrl', [ '$scope', 'Base64', function($scope, $Base64){
     $scope.person = {};
+    $scope.username = '';
+    $scope.password = '';
     $scope.identify = function(){
         //I want to read the form fields and call a rest api
         //curl -u $USERNAME:$PASSWORD -X GET https://www.pivotaltracker.com/services/v4/me
         //substitute the form values make the call and then convert the returned xml into 
         //a person object whose token is used for all subsequent requests.
-        $scope.person.username = angular.element('#username').val();
-        $scope.person.password = angular.element('#password').val();
-        alert('username: ' + $scope.person.username + '\npassword: ' + $scope.person.password);
+        $scope.username = angular.element('#username').val();
+        $scope.password = angular.element('#password').val();
+        if( $scope.username == '' || $scope.password == '' ) { return; }
+        var encoded = $Base64.encode($scope.username + ':' + $scope.password);
+        $http.defaults.headers.common.Authorization = 'Basic ' + encoded;
+        $http({
+            method: 'GET', 
+            url: 'https://www.pivotaltracker.com/services/v4/me', 
+            withCredentials: true
+        });
     };
   }]);
