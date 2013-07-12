6966a31421bf50fc6ae69547322aaf6870c66243
Ketema Harris <ketema@midnightoilconsulting.com>
2013-07-11 20:41:51 -0400

    Trying to pass form field values to a function
    [#53196411]
    
    Stuck trying to figure out how to simply pass form field values
    to an angular model (just becuse its a function doesn't mean its not
    a model right?) identify is a $scope variable that is just a function
    that alerts its paramaters.  I successfully used the ng-click directive
    and this function fires on the button click.  I can alert static string
    values no problem.  But when I try to pass the form field values by way
    of selector val() function I get null. Going to research if I am even
    approaching this correctly.

--- pub/js/controllers.js
+++ pub/js/controllers.js
@@ -3,6 +3,15 @@
 /* Controllers */
 
 angular.module('ptStats.controllers', []).
-  controller('ptAPICtrl', [ '$scope', function($scope){
-    $scope.users = "Go get the users from the rest api";
+  controller('ptWhoamICtrl', [ '$scope', function($scope){
+    $scope.identify = function(username, password){
+        //I want to read the form fields and call a rest api
+        //curl -u $USERNAME:$PASSWORD -X GET https://www.pivotaltracker.com/services/v4/me
+        //substitute the form values make the call and then convert the returned xml into 
+        //a user object whose token is used for all subsequent requests.
+        //
+        //First thing I have to figure out is how to attach this function to the click event of that
+        //form.
+        alert('username: '+username+'\npassword: '+password);
+    };
   }]);
