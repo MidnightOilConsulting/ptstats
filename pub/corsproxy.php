cee471edf124c98cadd5818ace2b6798f0427d4b
Ketema Harris <ketema@midnightoilconsulting.com>
2013-07-15 23:21:21 -0400

    [Finishes: #53196411]
    OK so the POC is done, now its time to refactor and do asom serious
    clean up.  We need to move the repetitive code up to have a base
    controller that calls the CORS proxy.  The php cors proxy needs to
    become a class for reusability.  Is it worth having the php class accept
    the REST syntax so that the JS controllers can use the $resource object
    provided by angular ?
    
    I also need to create a local data store for caching so that we can
    start answering some of questions.  Need to map each of the objects into
    related tables so we can apply some SQL.
    
    It would be nice to have an actual UI.
    
    We need a charting library to feed data to so we can get nice graphs.

--- pub/corsproxy.php
+++ pub/corsproxy.php
@@ -3,6 +3,17 @@
 $request = json_decode(http_get_request_body());
 
 $curl = curl_init($request->url);
-curl_setopt($curl, CURLOPT_USERPWD, $request->username . ":" . $request->password);
+
+if(!empty($request->username) && !empty($request->password))
+{
+    curl_setopt($curl, CURLOPT_USERPWD, $request->username . ":" . $request->password);
+}
+else if(!empty($request->token))
+{
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+        "X-TrackerToken: $request->token",
+    ));
+}
+
 curl_setopt($curl, CURLOPT_TIMEOUT, 30);
 curl_exec($curl);
--- pub/js/controllers.js
+++ pub/js/controllers.js
@@ -23,4 +23,20 @@ angular.module('ptStats.controllers', ['ptStats.services']).
             $scope.person = data.person;
         });
     };
+
+    $scope.getProjects = function(token){
+        $http.post('/corsproxy.php', {url:'http://www.pivotaltracker.com/services/v4/projects', token:token},
+            {
+                transformResponse: function(data) {
+                    var x2js = new X2JS();
+                    var json = x2js.xml_str2json( data );
+                    return json;
+                },
+            }
+        ).success(function(data){
+            $scope.projects = data.projects.project_asArray;
+            console.log($scope.projects);
+        });
+    };
+
   }]);
